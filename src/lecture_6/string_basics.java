6a48645b27636476d536fd6d43b17c74a5eb91de
prakash70428 <prakashmendish@gmail.com>
2024-04-10 18:50:36 +0530

    lecture-6 updated

---
+++ src/lecture_6/string_basics.java
@@ -0,0 +1,15 @@
+package lecture_6;
+import java.util.*;
+public class string_basics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Scanner scn = new Scanner(System.in);
+        int n = Integer.parseInt(scn.nextLine());
+        String str = scn.nextLine();
+        
+        System.out.println(n);
+        System.out.println(str);
+	}
+
+}
